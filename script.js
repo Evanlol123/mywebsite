6c54ef1403c83757409c8dac7cc21cfaaf55fab4
Evanlol123 <116678253+Evanlol123@users.noreply.github.com>
2024-10-25 12:23:37 -0400

    Update script.js

---
+++ script.js
@@ -0,0 +1,53 @@
+  document.addEventListener("DOMContentLoaded", function() {
+      document.body.classList.add('loaded'); // Add 'loaded' class to body when DOM is fully loaded
+  });
+
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 100,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+    },
+    "opacity": {
+      "value": 0.5,
+    },
+    "size": {
+      "value": 5,
+      "random": true
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 6,
+      "out_mode": "out"
+    }
+  },
+  "interactivity": {
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      }
+    }
+  },
+  "retina_detect": true
+});
